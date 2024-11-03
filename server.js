ed0bc3a6137614952eef854b665f02503bc937b5
Eldhose Babu <eldhosebabu197@gmail.com>
2024-11-03 12:48:06 -0500

    added a static html file
    this html file is returned when /test endpoint is hit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT_NUMBER = process.env.PORT || 4000
 
 server.get('/test', (request, respond) => {
     console.log(request)
-    respond.status(200).send("Connection hit root endpoint")
+    respond.status(200).sendFile(path.join(__dirname, 'index.html'))
 });
 
 server.listen(PORT_NUMBER, () => {
