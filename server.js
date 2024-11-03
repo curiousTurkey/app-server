c09ddf72125438269a8504f0502bfbea5c3d3104
Eldhose Babu <eldhosebabu197@gmail.com>
2024-11-03 18:51:34 -0500

    Added endpoint to search for movies

--- server.js
+++ server.js
@@ -4,12 +4,21 @@ const express = require("express");
 const path = require("path");
 const server = express(); //create an express server
 const PORT_NUMBER = process.env.PORT || 4000
+const OMDB_BASE_URL = `http://www.omdbapi.com/?`
 
-
-server.get('/test', (request, respond) => {
+server.get('/test', (request, response) => {
     console.log(request)
-    respond.status(200).sendFile(path.join(__dirname, 'index.html'))
+    response.status(200).send("API endpoints working as expected");
 });
+
+//server endpoint to get the api by searching for a movie title
+server.get('/searchMovies', (request, response) => {
+    const { title } = request.query
+    fetch(`${OMDB_BASE_URL}t=${title}&plot=short&apikey=${process.env.OMDB_API_KEY}`, {method: 'GET'}).then( response => response.json()).then((data) => {
+        response.send(data);
+    })
+});
+
 
 server.listen(PORT_NUMBER, () => {
     console.log("Server listening on port number: ", PORT_NUMBER);
